e139cf6f13a8e27352406ca2dd4243cd4f18d853
Your Name <you@example.com>
2025-11-07 12:20:07 +0000

    main.tf changes

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 provider "azurerm" {
   features {}
+  use_cli = true
 }
 
 data "azurerm_resource_group" "main" {
@@ -15,26 +16,20 @@ variable "tags" {
   }
 }
 
-resource "azurerm_app_service_plan" "main" {
+resource "azurerm_service_plan" "main" {
   name                = "${var.prefix}-asp"
   location            = data.azurerm_resource_group.main.location
   resource_group_name = data.azurerm_resource_group.main.name
-  kind                = "Linux"
-  reserved            = true
-
-  sku {
-    tier = "Basic"
-    size = "B1"
-  }
-
-  tags = var.tags
+  sku_name            = "S1"
+  os_type             = "Linux"
+  tags                = var.tags
 }
 
 resource "azurerm_app_service" "main" {
   name                = "${var.prefix}-appservice"
   location            = data.azurerm_resource_group.main.location
   resource_group_name = data.azurerm_resource_group.main.name
-  app_service_plan_id = azurerm_app_service_plan.main.id
+  app_service_plan_id = azurerm_service_plan.main.id
 
   tags = var.tags
 }
